33bb2689f8a375472b73c0c39ce2a77e15e516f4
Guillaume HARARI <guillaumeharari@hotmail.com>
2019-12-23 23:52:39 +0100

    Init and add a counter button

---
+++ index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  VrButton,
+} from 'react-360';
+import {Surface} from 'react-360-web';
+
+export default class Hello360 extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  _incrementCount = () => {
+    this.setState({count: this.state.count + 1});
+  };
+
+  render() {
+    return (
+
+      <View style={styles.panel}>
+        <VrButton
+          onClick={this._incrementCount}
+          style={styles.greetingBox}>
+          <Text style={styles.greeting}>
+            {`Count: ${this.state.count}`}
+          </Text>
+        </VrButton>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  panel: {
+    // Fill the entire surface
+    width: 1000,
+    height: 600,
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  greetingBox: {
+    padding: 20,
+    backgroundColor: '#000000',
+    borderColor: '#639dda',
+    borderWidth: 2,
+  },
+  greeting: {
+    fontSize: 30,
+  },
+});
+
+AppRegistry.registerComponent('Hello360', () => Hello360);
